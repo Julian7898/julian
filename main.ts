5eff94c661313111ee6d6a228f5cc968a76ee8b3
Julian7898 <meyerjulian654@yahoo.com>
2025-02-28 10:14:03 +0100

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,11 @@
+input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+	
+})
+basic.showString("Fussball", 150)
+basic.showIcon(IconNames.Yes, 50)
+basic.showNumber(20, 210)
+music.play(music.builtinPlayableSoundEffect(soundExpression.giggle), music.PlaybackMode.UntilDone)
+music.play(music.stringPlayable("G F G A - F E D ", 1000), music.PlaybackMode.LoopingInBackground)
+basic.forever(function () {
+	
+})
